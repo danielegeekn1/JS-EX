b1ed52dbe4f3c1f2ec1bcc56f7eb8d04af6c9006
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-09 20:16:54 +0200

    add while loops

--- scripts.js/loops.js
+++ scripts.js/loops.js
@@ -1,1 +1,7 @@
 //while loops 
+let counter = 0;
+while (counter < 10){
+    console.log ('valore di counter (while)', counter); //con questo si dice di mostrare la stringa fra gliapici e il valore counter
+    counter++;//con questo diciamo al browser di aggiungere sempre un valore a quello di counter, fino a raggiungimento loop va avanti
+    
+}
