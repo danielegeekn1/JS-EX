d7fdc1beace5057b2fc87317389e4a5f0e77e576
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-09 13:32:08 +0200

    add ex02

--- exercise-2.js
+++ exercise-2.js
@@ -5,7 +5,7 @@ function canPlay() {
     personName = 'George'; // Argh! Voglio che personName sia una variabile di sola lettura
   }
 
-  personName += ' plays football';
+   personName += ' plays football';
 
   console.log(personName);
 }
