ba43bf90588e3bfd55859595edc3b1669d45692e
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-09 15:00:35 +0200

    add some examples

--- scripts.js/typesconversion.js
+++ scripts.js/typesconversion.js
@@ -1,3 +1,11 @@
 //convertire una valore di una variabile da booleano a stringa
+ //dichiariamo prima il valore booleano alla variabile
 let BoolValue = true;
 console.log (typeof BoolValue);
+    //poi tramite questo passaggio possiamo convertire il valore della variabile da booleano  a stringa
+BoolValue = String(BoolValue);
+console.log (typeof BoolValue);
+
+//questa variabile ha come valore una stringa ( fra gli apici è considerata tale, per quanto numero interno è numero)
+const strValue = '42';
+console.log (typeof strValue);
