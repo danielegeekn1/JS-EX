a5e5345387828f18cb023dee57eed037687adf19
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-09 14:53:41 +0200

    add file conversione tipi

--- es-02/exercise-2.js
+++ es-02/exercise-2.js
@@ -1,11 +1,11 @@
 function canPlay() {
-  let personName = 'Paul';
+  const personName = 'Paul';
 
   if (true) {
     personName = 'George'; // Argh! Voglio che personName sia una variabile di sola lettura
   }
 
-  personName += ' plays football';
+   personName += ' plays football';
 
   console.log(personName);
 }
---
+++ scripts.js/typesconversion.js
@@ -0,0 +1,3 @@
+//convertire una valore di una variabile da booleano a stringa
+let BoolValue = true;
+console.log (typeof BoolValue);
