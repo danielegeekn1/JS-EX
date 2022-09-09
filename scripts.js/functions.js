477bafdfdf40c6cb8f215dac458952055bda9838
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-09 09:43:01 +0200

    add arguments & parameters

--- scripts.js/functions.js
+++ scripts.js/functions.js
@@ -1,3 +1,5 @@
+
+//function declaration
 function greet (){
     console.log('Hello there');
 }
@@ -5,3 +7,29 @@ function greet (){
 greet ();
 greet ();
 greet ();
+
+//function expression 
+//in this case we have a variable, that is equal to the function
+
+const speak = function (){
+    console.log ('Good morning');
+}; // function expression always have semi columns at the end 
+
+speak ();
+
+//arguments & parameters
+const talk = function (name)// name is what we call function parameter
+{
+    console.log('Hello there ${name}');
+};
+
+talk ('Mario');//mario in this case, is what we call the argument of the function
+
+//why ${name} is shown in terminal excatly like this and so it's not working??
+
+const talk2 = function (name, time){
+console.log('Good ${time} ${name}');
+};
+
+talk2 ('there', 'Daniele');
+//in this case we chosen to use 2 parameters for the same functions, that we invoked by using both of 2 arguments avaiable
