73bffb689773e6a7cd75c168a64a61ff8911e3f2
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-10 10:35:13 +0200

    ex12 done

--- exercise-2.js
+++ exercise-2.js
@@ -1,7 +1,12 @@
-const concat = function (arr1, arr2) {
+/*const concat = function (arr1, arr2) {
   return arr1.concat(arr2);
 }
 
-console.log(concat([2, 4], [5, 1]));
+console.log(concat([2, 4], [5, 1]));*/
 
 //prima variabile funzione, scritta come un'arrow function
+const concat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+}
+  
+console.log (concat([2, 4], [5, 1]));
