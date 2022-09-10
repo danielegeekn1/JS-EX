9017964ba93501fdab6359e937e74af5ceaf9e35
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-10 22:26:06 +0200

    ex 05 done

--- exercise-2.js
+++ exercise-2.js
@@ -2,10 +2,27 @@
 // manager => 1800
 // cto => 1800
 // developer => 1500
-// default => 1000
+// other => 1000
 function calculateSalary(role) {
-  // ...
+  switch (role){
+    case 'ceo':
+    return 2200;
+    
+    case 'manager':
+      return 1800;
+      
+    case 'cto':
+      return 1800;
+
+    case 'developer':
+      return 1500;
+
+    case 'other':
+      return 1000;
+
+  }
 }
+
 
 const ceoSalary = calculateSalary('ceo');
 const managerSalary = calculateSalary('manager');
