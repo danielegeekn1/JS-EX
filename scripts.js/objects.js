d86236c1d3e13272af3ff8485d63282cdfe39306
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-11 18:25:03 +0200

    object practice

--- scripts.js/objects.js
+++ scripts.js/objects.js
@@ -11,3 +11,20 @@ let hero={
 console.log(Object.values(hero));
 console.log(Object.keys(hero));
 console.log(Object.entries(hero));
+
+//esempio di oggetto 
+const person = {
+    name: ['Bob', 'Smith'],
+    age: 32,
+    bio() {
+      console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf() {
+      console.log(`Hi! I'm ${this.name[0]}.`);
+    }
+  };
+  console.log(Object.entries(person));//questa riproduce proprietà e valodi dell'oggetto, ma non quelli all'interno della funzione
+
+  console.log(person.bio());// questo può reinderizzare il risultato della funzione
+
+  
