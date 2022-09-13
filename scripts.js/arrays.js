630e70f53ed83d82b1798f046aaa17e08c83db0c
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-13 11:58:13 +0200

    add arrays info

--- scripts.js/arrays.js
+++ scripts.js/arrays.js
@@ -7,7 +7,12 @@ let heroes =['Batman', 'Joker', 'Enigmista'];
 let otherHeroes = new Array ('Batman', 'Joker', 'Enigmista');
 
 //utilizzo parola chiave 'pop' per rimuover un elemento di un'array da una collezione e spostarla in un'altra
-let superBad =['Batman', 'Joker', 'Enigmista'];
+let Heroes =['Batman', 'Joker', 'Enigmista'];
 
-let lastBad = superBad.pop();
+let lastHeroes = Heroes.pop();
+
+//metodo per aggiungere invece un'elemento ad una collezione di un'array con la parola chiave 'push'
+
+heroes.push('Green Arrow');//in questo caso per esempio stiamo aggiungendo ad heroes l'elemento 'Green Arrow'
+
 
