a58c228002fb8e02c5aa5e23bb1738cc8b2e204a
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-13 11:50:37 +0200

    array

---
+++ scripts.js/arrays.js
@@ -0,0 +1,13 @@
+//dichiarare un'array tramite una variabile
+
+let heroes =['Batman', 'Joker', 'Enigmista'];
+
+//dichiarare un'array tramite l'operatore new
+
+let otherHeroes = new Array ('Batman', 'Joker', 'Enigmista');
+
+//utilizzo parola chiave 'pop' per rimuover un elemento di un'array da una collezione e spostarla in un'altra
+let superBad =['Batman', 'Joker', 'Enigmista'];
+
+let lastBad = superBad.pop();
+
