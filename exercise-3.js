b9b1c28f02c63c85334604e55578bca62c740fc6
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-14 16:40:03 +0200

    ex21done

--- exercise-3.js
+++ exercise-3.js
@@ -11,7 +11,12 @@ const person1 = {
 
 // Argh! Nonostante abbia utilizzato Object.assign(), modificando la proprietà "address",
 // viene modificato il riferimento all'oggetto person1
-const person2 = Object.assign({}, person1);
+//const person2 = Object.assign({}, person1)
+//inizio deep copy con metodo 'JSON stringify
+//json stringify crea una stringa, json permette di riempirla, uso entrambi associandoli ad una variabile
+let person2 = JSON.parse(JSON.stringify(person1));
+
+//da ora le modifiche apportate alle proprietà della variabile person2 non avranno effetto su quelle di person1
 
 person2.address.region = 'Lombardia';
 person2.address.city = 'Milan';
