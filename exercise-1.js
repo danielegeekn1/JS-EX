58df9b261df61d555443f21c1b77fc8b593fb787
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-15 17:41:18 +0200

    ex22 done

--- exercise-1.js
+++ exercise-1.js
@@ -1,10 +1,30 @@
-const person = {
+/*const person = {
   firstName: 'John',
   lastName: 'Doe',
   age: 25,
   job: 'Web Developer',
   // ...
-}
+}*/
+
+//inserire i metodi 'fullName' e 'info' all'interno dell'oggetto person 
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age:25,
+  job: 'Web Developer',
+
+  fullName(){
+    return this.firstName + " " + this.lastName;
+  },
+  info (){
+    return this.age + " " + this.job;
+  }
+ 
+
+  }
+ 
+
 
 console.log(person.fullName()); // John Doe
 console.log(person.info()); // John Doe, 25 - Web Developer
+
