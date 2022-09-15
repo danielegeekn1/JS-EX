6f3341c93e5f2afdb90cb6d34b1fd4e39aab244f
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-15 15:26:46 +0200

    add method

--- exercise-2.js
+++ exercise-2.js
@@ -1,6 +1,14 @@
 function Person(firstName, lastName) {
-  // ...
+
+function fullName (){
+  return `${firstName} ${lastName}`;
+  
 }
+return fullName;
+}
+
+let john = new Person (John, Doe);
+let simon = new Person (Simon, Collins);
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
