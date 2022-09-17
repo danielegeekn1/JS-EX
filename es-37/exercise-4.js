a225ece0ba5a727fe16375ce7838b4246e872174
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-17 09:43:46 +0200

    first part

--- es-37/exercise-4.js
+++ es-37/exercise-4.js
@@ -1,3 +1,19 @@
+//creazione classe
+class BankAccount  {
+     fund = [];
+     deposit (funds){
+        this.fund.push(funds);
+     }
+     withdraw (funds){
+        this.fund.pop(funds);
+     }
+     view (){
+        return this.fund;
+     }
+    }
+
+
+
 const bankAccount = new BankAccount(1000);
 bankAccount.deposit(500);
 bankAccount.deposit(200);
