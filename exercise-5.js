fd8bc79dd8b83bd65582850477f9398e1aff1d91
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-17 15:05:42 +0200

    first commit

---
+++ exercise-5.js
@@ -0,0 +1,30 @@
+function calculateAverageAge(persons) {
+  // ...
+  sum = 0;
+
+for (i=0; i <persons.length; i++){
+  while(i <persons.length){
+    sum = sum + persons.length++;
+
+  }
+  return sum / persons.length;
+}
+
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
