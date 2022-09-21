3ff13ee0f6235c864ae69ef03ea06aaeb0ad2ba1
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-21 10:50:05 +0200

    ex17 done

--- exercise-4.js
+++ exercise-4.js
@@ -1,6 +1,31 @@
 function calculate() {
   // ...
+ 
+  const calculator ={
+            base : 0,
+            add(num){
+              this.base = this.base + num;
+              return this;
+            },
+            multiply(num){
+              this.base = this.base * num;
+              return this;
+            },
+            sub(num){
+              this.base = this.base - num;
+              return this;
+            },
+            divide(num){
+              this.base = this.base / num;
+              return this;
+            },
+            printResult(){
+              console.log(this.base);
+            }
+  }
+  return calculator;
 }
 
 const calculator = calculate();
 calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+
