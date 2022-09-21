b65233cab0add7880b8266839115518417111f52
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-21 14:09:55 +0200

    ex done

--- exercise-4.js
+++ exercise-4.js
@@ -1,10 +1,15 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
 };
 
-for (let item of Object.entries(person)) {
-  console.log(`${item[0]}: ${item[1]}`);
+/*for (let item of Object.entries(person)) {
+  console.log(`${item[0]}: ${item[1]}`); 
+}*/
+let { id, firstName, lastName, age } = person;
+
+for (let [key, value] of Object.entries(person)) {
+  console.log(`${key}: ${value}`);
 }
