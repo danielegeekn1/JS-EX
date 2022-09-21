cf422b9b5b7b961e3eda6db94f3059741ad9868c
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-21 19:05:33 +0200

    ex 56 done

--- exercise-4.js
+++ exercise-4.js
@@ -1,6 +1,11 @@
-function sum(num1, num2, num3) {
+/*function sum(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
+const numbers = [1, 2, 3];
+console.log(sum(numbers[0], numbers[1], numbers[2]));*/
+function sum(...num) {
+  return num;
+}
 const numbers = [1, 2, 3];
 console.log(sum(numbers[0], numbers[1], numbers[2]));
