7501ee17dd3caa9d1ec92f73dc58d38cfc16fcb6
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-21 18:52:42 +0200

    ex55 fatto

--- exercise-3.js
+++ exercise-3.js
@@ -1,4 +1,9 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-numberStore.push(newNumber);
+/*numberStore.push(newNumber);*/
+
+//associare a newNumber un valore iterabile
+//let newNumber = [3];
+numberStore = [...numberStore, newNumber];
+
 console.log(numberStore);
