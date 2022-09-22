27d8d9633afba4e5abfb7df62163452629b13cfa
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-22 17:35:19 +0200

    ex 23 done

--- exercise-1.js
+++ exercise-1.js
@@ -1,7 +1,12 @@
 const person = {
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-}
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+Object.keys(person).forEach((key) => {
+  console.log(key, person[key]);
+});
 
 // Print values of person using Object.keys
+
+console.log(Object.keys(person));
