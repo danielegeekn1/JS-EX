66c35bf1fc74b2d8855696e7e50b773e52bcb04c
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-22 17:39:14 +0200

    ex 29 rifatto

--- exercise-1.js
+++ exercise-1.js
@@ -1,18 +1,25 @@
 function adultFilter(persons) {
   // ...
+  const Over18 = [];
+  for (i = 0; i < persons.length; i++) {
+    if (persons[i].age >= 18) {
+      Over18.push(persons[i]);
+    }
+  }
+  return Over18;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 const adults = adultFilter(persons);
