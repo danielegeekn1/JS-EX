5802bef3d3920e87b2b7b7f330758d38e6ac3dc5
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-22 16:48:05 +0200

    ex 28 done

--- exercise-2.js
+++ exercise-2.js
@@ -1,9 +1,13 @@
 const order = {};
 
-/*if (order && order.customer && order.customer.address && !order.customer.address.city) {
-  console.log('City is required');
-}*/
-
+/*if (
+  order &&
+  order.customer &&
+  order.customer.address &&
+  !order.customer.address.city
+) */ {
+}
 
 // il codice si può semplificare grazie all'utilizzo dell'optional chaining in questo modo
 console.log(order?.customer?.address?.city);
+console.log("City is required");
