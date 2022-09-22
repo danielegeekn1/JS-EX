4e43f8bd41bf7131e10247222a778138bd376c01
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-22 17:45:16 +0200

    ex 30 done

--- exercise-2.js
+++ exercise-2.js
@@ -1,18 +1,23 @@
 function nicknameMap(persons) {
   // ...
+  let alias = [];
+  for (let a = 0; a < persons.length; a++) {
+    alias.push(persons[a].name + "-" + persons[a].age);
+  }
+  return alias;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 const nicknames = nicknameMap(persons);
