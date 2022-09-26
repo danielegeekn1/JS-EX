8a03eef9603dbead635185d3063f9f542fedf843
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-26 16:39:17 +0200

    new branch commit

--- exercise-1.js
+++ exercise-1.js
@@ -1,26 +1,44 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
 function fetchPersonById(id) {
   // code here
+  return new Promise((resolve) => {
+    id = persons.id;
+    if (persons.id === 1) {
+      resolve(persons[0]);
+    } else if (persons.id === 2) {
+      resolve(persons[1]);
+    } else if (persons.id === 3) {
+      resolve(persons[2]);
+    }
+  });
 }
 
+async function alias() {
+  let firstId = await fetchPersonById(1);
+  console.log(firstId);
+  let secondId = await fetchPersonById(2);
+  console.log(secondId);
+  let thirdId = fetchPersonById(3);
+  console.log(thirdId);
+}
 // code here
