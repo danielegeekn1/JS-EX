43616e0c21c3d458b634ca7bd52ad6b7ff30a4d9
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-26 17:29:58 +0200

    ex 58 done

--- exercise-2.js
+++ exercise-2.js
@@ -1,28 +1,44 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
-function fetchPersonById(id) {
+/*function fetchPersonById(id) {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(persons.find(item => item.id === id)), 1000);
+    setTimeout(() => resolve(persons.find((item) => item.id === id)), 1000);
+  });
+}*/
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    persons.find((item) => item.id === id);
+    if (id === 1) {
+      resolve(persons[0]);
+    } else if (id === 2) {
+      resolve(persons[1]);
+    } else if (id === 3) {
+      resolve(persons[2]);
+    } else {
+      reject("Promise rejected");
+    }
   });
 }
-
+fetchPersonById(1).then((person) => console.log(person));
 fetchPersonById(2).then((person) => console.log(person));
+fetchPersonById(3).then((person) => console.log(person));
+fetchPersonById(4).catch((err) => console.log(err));
