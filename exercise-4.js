474986fdcdf38631a17b8a49365047f7ff9d6517
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-29 11:00:39 +0200

    ex 32 done

--- exercise-4.js
+++ exercise-4.js
@@ -1,57 +1,75 @@
 function uncompletedNotes(notes) {
   // ...
+
+  const newNote = [];
+
+  notes.forEach((elem) => {
+    let todos = elem.todos;
+    let status = true;
+    todos.forEach((elem2) => {
+      let done = elem2.done;
+      if (done === false) {
+        status = false;
+      }
+    });
+    if (status === false) {
+      newNote.push(elem);
+    }
+  });
+  return newNote;
 }
 
+//console.log(uncompletedNotes());
 const notes = [
   {
     id: 1,
-    description: 'Workout program',
+    description: "Workout program",
     todos: [
       {
         id: 1,
-        name: 'Push ups - 10 x 3',
-        done: false
+        name: "Push ups - 10 x 3",
+        done: false,
       },
       {
         id: 2,
-        name: 'Abdominals - 20 x 3',
-        done: true
+        name: "Abdominals - 20 x 3",
+        done: true,
       },
       {
         id: 3,
-        name: 'Tapis Roulant - 15min',
-        done: true
-      }
-    ]
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
   },
   {
     id: 2,
-    description: 'Front-end Roadmap',
+    description: "Front-end Roadmap",
     todos: [
       {
         id: 1,
-        name: 'Learn HTML',
-        done: true
+        name: "Learn HTML",
+        done: true,
       },
       {
         id: 2,
-        name: 'Learn CSS',
-        done: true
+        name: "Learn CSS",
+        done: true,
       },
       {
         id: 3,
-        name: 'Learn JavaScript',
-        done: true
+        name: "Learn JavaScript",
+        done: true,
       },
       {
         id: 4,
-        name: 'Learn Angular',
-        done: true
-      }
-    ]
-  }
-]
+        name: "Learn Angular",
+        done: true,
+      },
+    ],
+  },
+];
 
 const notesInProgress = uncompletedNotes(notes);
-console.log('All notes: ', notes);
-console.log('Notes In Progress: ', notesInProgress);
+console.log("All notes: ", notes);
+console.log("Notes In Progress: ", notesInProgress);
