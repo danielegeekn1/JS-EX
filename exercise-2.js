5839abd68f1ceceeca1a858940ecb336557fb15d
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-09-30 12:19:35 +0200

    ex 26 done

--- exercise-2.js
+++ exercise-2.js
@@ -1,14 +1,18 @@
 function Person(firstName, lastName) {
-
-function fullName (){
-  return `${firstName} ${lastName}`;
-  
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = () => `${this.firstName} ${this.lastName}`;
 }
-return fullName;
-}
+const person = {
+  firstName: "name",
+  lastName: "surname",
+};
 
-let john = new Person (John, Doe);
-let simon = new Person (Simon, Collins);
+/*let john = new Person(John, Doe);
+let simon = new Person(Simon, Collins);*/
+
+let john = new Person("John", "Doe");
+let simon = new Person("Simon", "Collins");
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
