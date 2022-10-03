c87d1d5864a1037afc23b016f322d8ea02437ba0
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-10-03 17:56:15 +0200

    ex 59 done

--- exercise-3.js
+++ exercise-3.js
@@ -1,36 +1,40 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
 function fetchPersonById(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const person = persons.find(item => item.id === id);
-      
+      const person = persons.find((item) => item.id === id);
+
       if (person) {
-        return resolve(JSON.stringify(person));
+        return resolve(JSON.parse(JSON.stringify(person)));
+        //return resolve(JSON.stringify(person));
       }
 
       return reject(`Person with id: ${id} doesn't exist`);
     }, 1000);
   });
 }
+
+let JsonFile = fetchPersonById(1);
+JsonFile.then((person) => console.log(person)).catch((err) => console.log(err));
 
 // core here
